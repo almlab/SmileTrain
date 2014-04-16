ef9efe03ee3af8ed515ffc30eee365d3c67a23f0
swo <swo@mit.edu>
2014-04-16 18:37:08 -0400

    remove_primers: refactored
    renamed primer.py to avoid clashes with obvious variable name

--- remove_primers.py
+++ remove_primers.py
@@ -1,52 +1,71 @@
-import argparse, primer, sys
+import argparse, itertools, sys
+import usearch_python.primer_tools
 from util import *
 
-def mismatches(seq, subseq, w):
-    # calculate mismatches between seq and subseq with window size w
+def mismatches(seq, primer, w):
+    '''
+    Calculate mismatches between a sequence and primer with window size w
+    '''
+
     I = 0
     D = len(seq)
     for i in range(w):
-        d = primer.MatchPrefix(seq[i:], subseq)
+        d = usearch_python.primer_tools.MatchPrefix(seq[i:], primer)
         if d < D:
             I = i
             D = d
     return [I, D]
 
+def remove_primers(fastq, primer, max_primer_diffs):
+    '''
+    Remove well-matched primers from sequences in a fastq file
 
-def parse_args():
+    Parameters
+    fastq : sequence or iterator of strings
+        lines in the fastq file
+    primer : string
+        the primer sequence to be removed
+    max_primer_diffs : int
+        the maximum number of mismatches allowed before throwing out the read
+
+    Returns nothing. All output is delivered with 'print'.
+    '''
+
+    primer_length = len(primer)
+
+    # loop over sets of 4 lines at a time
+    for at_line, seq_line, plus_line, quality_line in itertools.izip(*[iter(fastq)] * 4):
+        # chomp all newlines
+        at_line = at_line.rstrip()
+        seq_line = seq_line.rstrip()
+        plus_line = plus_line.rstrip()
+        quality_line = quality_line.rstrip()
+
+        # check that the two lines with identifiers match our expectations
+        assert(at_line.startswith('@'))
+        assert(plus_line.startswith('+'))
+
+        # check that the sequence and quality lines have the same number of nucleotides
+        assert(len(seq_line) == len(quality_line))
+
+        # find the primer position in the sequence
+        primer_start_index, primer_diffs = mismatches(seq_line, primer, 15)
+
+        # if we don't find a good match, move on. otherwise, trim the sequence and the
+        # quality line and print a new fastq entry.
+        if primer_diffs <= max_primer_diffs:
+            primer_end_index = primer_start_index + primer_length
+            trimmed_seq = seq_line[primer_end_index:]
+            trimmed_quality = quality_line[primer_end_index:]
+            print "\n".join([at_line, trimmed_seq, '+', trimmed_quality])
+
+
+if __name__ == '__main__':
     # parse command line arguments
-    parser = argparse.Argument_Parser()
-    parser.add_argument('-i', default = '', help = 'input fasta file')
-    parser.add_argument('-p', default = '', help = 'primer sequence')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fastq', help='input fastq file')
+    parser.add_argument('primer', help='primer sequence')
+    parser.add_argument('-m', '--max_primer_diffs', default=0, type=int, help='maximum number of nucleotide mismatches in the primer (default: 0)')
     args = parser.parse_args()
 
-
-def remove_primers():
-    # remove primers
-
-
-
-# Read input arguments
-
-fasta = sys.argv[1]
-prime = sys.argv[2]
-MAX_PRIMER_DIFFS = int(sys.argv[3])
-PL = len(prime)
-
-# Remove primers
-k = 0
-for line in open(fasta):
-    line = line.rstrip()
-    k += 1
-    if k%4 == 1:
-        r = line[1:]
-    elif k%4 == 2:
-        seq = line
-        I, D = mismatches(seq, prime, 15)
-        if D > MAX_PRIMER_DIFFS:
-            r = ''
-        else:
-            seq = seq[I+PL:]
-    elif r != '' and k%4 == 0:
-        qua = line[I+PL:]
-        print '@%s\n%s\n+\n%s' %(r, seq, qua)
+    remove_primers(open(args.fastq), args.primer, args.max_primer_diffs)
