4c71fe95338aa9df025af5abd5d734af99a2f209
swo <swo@mit.edu>
2014-04-18 10:55:46 -0400

    refactored derep_fulllength
    The original script apparently had some functionality for a "uc" file.
    This file showed the read IDs that mapped to the each sequence:
    
    ACGT  foo    bar    baz
    CATC  lorem  ipsum  qualcum
    
    This doesn't seem to appear in downstream analysis, so I dropped it from
    the refactored script.

--- derep_fulllength.py
+++ derep_fulllength.py
@@ -1,35 +1,110 @@
+'''
+Dereplicate a fasta file. Read through all the entries, finding reads with identical
+sequences. Write out a new fasta file with IDs that keep track of the abundance of
+each sequence.
+
+input like
+    >foo
+    CATCAT
+    >bar
+    TATA
+    >baz
+    AGGAGG
+    >lorem
+    TATA
+    >ipsum
+    CATCAT
+    >qualcum
+    CATCAT
+
+with a minimum number of reads
+    2
+
+becomes output like
+    >otu1;size=3
+    CATCAT
+    >otu2;size=2
+    TATA
+
+The output file is sorted in order of decreasing abundance. Sequences with a minimum
+number of counts are dropped.
+'''
+
 import sys
 
-f = sys.argv[1]
-o = sys.argv[2]
-u = sys.argv[3]
+def fasta_entries(lines):
+    '''
+    Yield [id, sequence] pairs from a fasta file. Sequence allowed to run over multiple
+    lines.
 
-out = open(o, 'w')
-uc = open(u, 'w')
+    Parameters
+    lines : sequence or iterator of strings
+        lines from the fasta file
 
-fst = {}
+    Yields [id (without the >), sequence] pairs
+    '''
 
-for line in open(f):
-    line = line.rstrip()
-    if line.startswith('>'):
-        sid = line[1:]
-    else:
-        seq = line
-        if seq not in fst:
-            fst[seq] = [sid]
+    entry = ''
+    for line in lines:
+        line = line.rstrip()
+        if line.startswith('>'):
+            if entry != '':
+                yield [sid, sequence]
+            else:
+                sid = line[1:]
+                sequence = ''
         else:
-            fst[seq].append(sid)
+            sequence += line
 
-i = 0
-for seq in sorted(fst.keys(), key=lambda x: len(fst[x]), reverse=True):
+    yield [sid, sequence]
 
-    if len(fst[seq]) < 2:
-        continue
+def sequence_abundances(entries):
+    '''fasta entries to dictionary {sequence: # reads}'''
+    abundance_map = {}
+    for sid, sequence in entries:
+        if sequence not in abundance_map:
+            abundance_map[sequence] = 1
+        else:
+            abundance_map[sequence] += 1
 
-    i += 1
-    otu = 'seq%d' %(i)
-    out.write( '>%s;size=%d\n%s\n' %(otu, len(fst[seq]), seq))
-    uc.write('%s\t%s\n' %(otu, '\t'.join(fst[seq])))
+    return abundance_map
 
-out.close()
-uc.close()
+def sorted_abundant_keys(abundance_map, minimum_counts):
+    '''
+    Given a dictionary {key: number}, get a list of keys. The keys are sorted so that
+    d[key] are numbers in decreasing order and d[key] is at least minimum counts.
+
+    Parameters
+    abundance_map : dictionary
+        a dictionary {key: number}
+    minimum_counts : int
+        minimum number to avoid being dropped
+
+    Returns
+    keys : list
+        keys in decreasing order with numbers above the minimum
+    '''
+
+    sorted_keys = sorted(abundance_map.keys(), key=lambda k: abundance_map[k], reverse=True)
+    filtered_sorted_keys = [k for k in sorted_keys if abundance_map[k] >= minimum_counts]
+
+    return filtered_sorted_keys
+
+
+if __name__ == '__main__':
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='input fasta file')
+    parser.add_argument('output', help='output fasta file')
+    parser.add_argument('-m', '--minimum_counts', type=int, default=2, help='minimum times a sequence is included, otherwise it gets thrown out (default: 2)')
+    args = parser.parse_args()
+
+    with open(args.input, 'r') as f:
+        abundance_map = sequence_abundances(fasta_entries(f))
+
+    sorted_seqs = sorted_abundant_keys(abundance_map, args.minimum_counts)
+
+    with open(args.output, 'w') as f:
+        for otu_i, seq in enumerate(sorted_seqs):
+            f.write('>otu%s;size=%d\n' % (otu_i, abundance_map[seq]))
+            f.write('%s\n' %(seq))
