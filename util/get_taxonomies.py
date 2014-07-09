b19ba2d3265d6c3d8ad2c15b703ee0f38a63f1df
swo <swo@mit.edu>
2014-07-09 12:40:12 -0400

    util: added utilities for taxonomy assignment

--- util/get_taxonomies.py
+++ util/get_taxonomies.py
@@ -3,9 +3,11 @@
 '''
 Get greengenes taxonomies. Given an otu table with otu ids in the first column, search through
 the greengenes taxonomy list. Output the taxonomies in order.
+
+If the input database is a pickle, just load that dictionary.
 '''
 
-import sys, argparse, re
+import sys, argparse, re, cPickle as pickle
 
 def table_ids(fn):
     '''get the otu ids from the otu table with filename fn'''
@@ -33,6 +35,14 @@ def matching_fields(fn, ids):
     taxa = [d[i] for i in ids]
     return taxa
 
+def pickled_taxa(fn, ids):
+    '''get matching taxa from a pickled tax dictionary'''
+    with open(fn, 'rb') as f:
+        d = pickle.load(f)
+    
+    taxa = [d[i] for i in ids]
+    return taxa
+
 
 if __name__ == '__main__':
     # parse command line arguments
@@ -42,5 +52,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     ids = table_ids(args.input)
-    taxa = matching_fields(args.db, ids)
+
+    # check if the database file ends in .pkl or .pickle
+    # if it is, used a pickled dictionary
+    # otherwise, just search line by line
+    if re.search("\.(pkl|pickle)$", args.db):
+        taxa = pickled_taxa(args.db, ids)
+    else:   
+        taxa = matching_fields(args.db, ids)
+        
     print "\n".join(taxa)
