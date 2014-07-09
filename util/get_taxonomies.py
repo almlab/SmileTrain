aef5adb38ac577914529d0cb070eee339c86a08c
swo <swo@mit.edu>
2014-07-09 12:58:49 -0400

    util: improved taxonomy getter

--- util/get_taxonomies.py
+++ util/get_taxonomies.py
@@ -19,7 +19,7 @@ def table_ids(fn):
 
     return ids
 
-def matching_fields(fn, ids):
+def taxa_dictionary(fn, ids):
     '''get the second field in lines whose first fields match ids'''
     # populate a hash otu_id => taxonomy
     d = {}
@@ -31,17 +31,7 @@ def matching_fields(fn, ids):
             if otu in ids:
                 d[otu] = tax
 
-    # get the taxonomies in the order that the ids were given
-    taxa = [d[i] for i in ids]
-    return taxa
-
-def pickled_taxa(fn, ids):
-    '''get matching taxa from a pickled tax dictionary'''
-    with open(fn, 'rb') as f:
-        d = pickle.load(f)
-    
-    taxa = [d[i] for i in ids]
-    return taxa
+    return d
 
 
 if __name__ == '__main__':
@@ -49,6 +39,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input', help='input otu table')
     parser.add_argument('db', help='input database')
+    parser.add_argument('--no_match_id', default='no_match', help='OTU ID for no match (default "no_match")')
+    parser.add_argument('--no_match_tax', default='k__; p__; c__; o__; f__; g__; s__', help='taxonomy for unmatched OTU ID (default is QIIME taxonomy format')
     args = parser.parse_args()
 
     ids = table_ids(args.input)
@@ -57,8 +49,11 @@ if __name__ == '__main__':
     # if it is, used a pickled dictionary
     # otherwise, just search line by line
     if re.search("\.(pkl|pickle)$", args.db):
-        taxa = pickled_taxa(args.db, ids)
+        with open(fn, 'rb') as f:
+            d = pickle.load(f)
     else:   
-        taxa = matching_fields(args.db, ids)
+        d = taxa_dictionary(args.db, ids)
         
-    print "\n".join(taxa)
+    d[args.no_match_id] = args.no_match_tax
+        
+    print "\n".join([d[i] for i in ids])
