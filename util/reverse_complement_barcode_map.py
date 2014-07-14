8f1b2c0840fb03ee6ce8a993b507b8cd35ce3b42
swo <swo@mit.edu>
2014-07-14 12:25:58 -0400

    tools: added reverse complement barcoding

---
+++ util/reverse_complement_barcode_map.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+'''
+Take the reverse complements of the barcodes to make a new mapping file.
+'''
+
+import argparse, sys, csv
+import Bio.Seq
+
+
+def reverse_complement_map(m):
+    '''[['sample1', 'AAA'], ...] -> [['sample1', 'TTT'], ...]'''
+    return [[sample, Bio.Seq.Seq(code).reverse_complement()] for sample, code in m]
+
+
+if __name__ == '__main__':
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('barcodes', help='input barcode mapping')
+    parser.add_argument('--output', '-o', default=sys.stdout, type=argparse.FileType('w'), help='output mapping file (default stdout)')
+    args = parser.parse_args()
+    
+    with open(args.barcodes) as f:
+        r = csv.reader(f, delimiter='\t')
+        old_map = [l for l in r]
+        assert(len(old_map[0]) == 2)
+        
+    new_map = reverse_complement_map(old_map)
+    
+    w = csv.writer(args.output, delimiter='\t', quoting=csv.QUOTE_NONE)
+    w.writerows(new_map)
