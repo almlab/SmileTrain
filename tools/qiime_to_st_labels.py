8acaef481953b916bfd7be7464dd3edaec6fa1a2
swo <swo@mit.edu>
2014-08-18 12:24:59 -0700

    qiime label: fixed

--- tools/qiime_to_st_labels.py
+++ tools/qiime_to_st_labels.py
@@ -2,33 +2,34 @@
 
 '''
 Convert qiime labels to smile train labels
+
+e.g., ">samplename_123 lots of garbage stuff" -> ">sample=name;123"
 '''
 
 import argparse, re, sys
+from Bio import SeqIO
 from SmileTrain import util
 
-def convert_qiime_label(fasta_entries):
+def convert_qiime_label(record):
     ''' ">samplename_123 lots of garbage stuff" -> ">samplename;123" '''
-    for sid, sequence in fasta_entries:
-        # split into "samplename" and "123"
-        m = re.match("(.+)_(\d+)", sid)
-        if m is None: raise RuntimeError("line %s did not parse" %(sid))
-        sample_name, sequence_number = m.groups()
+    # split into "samplename" and "123"
+    m = re.match("(.+)_(\d+)", record.id)
+    if m is None: raise RuntimeError("record id '%s' did not parse" %(sid))
+    sample_name, sequence_number = m.groups()
         
-        # create a new sid
-        new_sid = "sample=%s;%s" %(sample_name, sequence_number)
+    # create a new id and throw out the other junk
+    record.id = "sample=%s;%s" %(sample_name, sequence_number)
+    record.description = ''
         
-        yield new_sid, sequence
+    return record
     
 
 if __name__ == '__main__':
     # parse command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('input', help='input fasta')
+    parser.add_argument('fasta', help='input fasta')
     parser.add_argument('--output', '-o', default=sys.stdout, type=argparse.FileType('w'), help='output fasta (default stdout)')
     args = parser.parse_args()
     
-    with open(args.input) as f:
-        for sid, seq in convert_qiime_label(util.fasta_entries(f)):
-            args.output.write(">%s\n%s\n" %(sid, seq))
-        
+    for record in SeqIO.parse(args.fasta, 'fasta'):
+        SeqIO.write(convert_qiime_label(record), args.output, 'fasta')
