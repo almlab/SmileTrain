e038ce10d726f147212a0295edaecd5cc13cac38
swo <swo@mit.edu>
2014-09-11 14:51:58 -0400

    remove_primers: added --output option

--- remove_primers.py
+++ remove_primers.py
@@ -30,9 +30,10 @@ if __name__ == '__main__':
     parser.add_argument('primer', help='primer sequence')
     parser.add_argument('-m', '--max_primer_diffs', default=0, type=int, help='maximum number of nucleotide mismatches in the primer (default: 0)')
     parser.add_argument('-l', '--log', default=None, type=str, help='log file for successes, failures, and time elapsed')
+    parser.add_argument('--output', '-o', default=sys.stdout, type=argparse.FileType('w'), help='output fastq (default stdout)')
     args = parser.parse_args()
 
-    r = util_primer.PrimerRemover(args.fastq, args.primer, args.max_primer_diffs)
+    r = util_primer.PrimerRemover(args.fastq, args.primer, args.max_primer_diffs, out=args.output)
     r.print_entries()
 
     if args.log is not None:
