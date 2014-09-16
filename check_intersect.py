344b6140805eafbf3e1afc3bbe93fde042a3312f
swo <swo@mit.edu>
2014-09-16 11:04:01 -0400

    check intersect: counts line numbers first

--- check_intersect.py
+++ check_intersect.py
@@ -5,11 +5,21 @@ Compare the forward and reverse fastq files. Make sure that there are pairs of f
 reverse reads that are appropriately labeled.
 '''
 
-import sys, argparse, itertools
+import sys, argparse, itertools, subprocess
 from Bio import SeqIO
 import util
 
+def n_lines(fn):
+    '''count number of lines in fn using wc'''
+    return int(subprocess.check_output(['wc', '-l', fn]).split()[0])
+
+def check_for_matched_line_numbers(f1, f2):
+    '''assert that two files have the same number of lines'''
+    if n_lines(f1) != n_lines(f2):
+        raise RuntimeError("fastqs have unequal numbers of lines")
+
 def check_matching_fastq_ids(forward_id, reverse_id):
+    '''check that reads are whatever/1 and whatever/2'''
     if not forward_id.endswith("/1"):
         raise RuntimeError("fastq id not correct direction: '%s' should end in 1" % forward_id)
 
@@ -20,6 +30,7 @@ def check_matching_fastq_ids(forward_id, reverse_id):
         raise RuntimeError("fastq ids did not match: '%s' and '%s'" % (forward_id, reverse_id))
 
 def check_for_paired_ids(forward_fastq, reverse_fastq):
+    '''check each pair of fastq records for paired read ids'''
     for forward_record, reverse_record in itertools.izip(SeqIO.parse(forward_fastq, 'fastq'), SeqIO.parse(reverse_fastq, 'fastq')):
         check_matching_fastq_ids(forward_record.id, reverse_record.id)
 
@@ -31,4 +42,5 @@ if __name__ == '__main__':
     parser.add_argument('reverse', help='reverse reads fastq')
     args = parser.parse_args()
 
+    check_for_matched_line_numbers(args.forward, args.reverse)
     check_for_paired_ids(args.forward, args.reverse)
