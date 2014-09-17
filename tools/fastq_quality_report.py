fee9937d655babaf79e89dee1856281f76e9c48f
swo <swo@mit.edu>
2014-09-17 14:55:39 -0400

    fastq quality report: not a memory hog now

--- tools/fastq_quality_report.py
+++ tools/fastq_quality_report.py
@@ -4,14 +4,19 @@
 Read a fastq and output the distribution of quality scores.
 '''
 
-import argparse, sys, os
+import argparse, sys, os, textwrap
 sys.path.append(os.path.normpath(os.path.abspath(__file__) + '/../..'))
 from SmileTrain import util
 from Bio import SeqIO
 
 import numpy as np
 
+# range of possible read quality scores
+min_q = 1
+max_q = 41
+
 def boxplot_stats(a):
+    '''given an array, return a dictionary with some stats useful for box plotting'''
     out = {}
     out['Q1'], out['Q2'], out['Q3'] = np.percentile(a, [25, 50, 75])
     iqr = out['Q3'] - out['Q1']
@@ -32,22 +37,54 @@ def boxplot_stats(a):
 
     return out
 
+def empty_position():
+    '''empty dictionary for holding quality score counts'''
+    return {x: 0 for x in range(min_q, max_q+1)}
+
+def quality_dictionary_to_array(d):
+    '''{1 => 3, 2 => 1} -> [1, 1, 1, 2]'''
+    o = []
+    for quality, count in d.items():
+        o += [quality]*count
+
+    return np.array(o)
+
 def fastq_quality_dist(fastq):
-    record_qualities = [record.letter_annotations['phred_quality'] for record in SeqIO.parse(fastq, 'fastq')]
+    '''
+    Read a fastq to create a list of dictionaries. The first dictionaries is {quality => counts}
+    for the first position in the reads, etc.
+    '''
 
-    # get the maximum read length
-    max_len = max([len(x) for x in record_qualities])
+    position_qualities = []
 
-    # pad the qualities with nan's
-    for q in record_qualities:
-        q += [np.nan] * (max_len - len(q))
+    for record in SeqIO.parse(fastq, 'fastq'):
+        quality = record.letter_annotations['phred_quality']
 
-    position_qualities = np.vstack(tuple(record_qualities)).T
-    position_stats = [boxplot_stats(position) for position in position_qualities]
+        while len(quality) > len(position_qualities):
+            position_qualities.append(empty_position())
+
+        for q, position in zip(quality, position_qualities):
+            if q > max_q or q < min_q:
+                raise RuntimeError("Quality %d outside of expected range (%d, %d)" %(min_q, max_q))
+
+            position[q] += 1
+
+    return position_qualities
+
+def fastq_quality_stats(fastq):
+    '''
+    Read a fastq to create a list of stats dictionaries. Every item in the list is for one
+    position amongst the reads (e.g., first item for first bp across all reads).
+    '''
+
+    position_qualities = fastq_quality_dist(fastq)
+    position_stats = [boxplot_stats(quality_dictionary_to_array(pq)) for pq in position_qualities]
 
     return position_stats
 
-def boxplot_line(stats, plot_min=33, plot_max=73):
+
+def boxplot_line(stats, plot_min=min_q, plot_max=max_q):
+    '''given statistics, write out a horizontal line in the boxplot'''
     out = "|"
     for i in range(plot_min, plot_max+1):
         if i < stats['lower whisker']:
@@ -69,19 +106,28 @@ def boxplot_line(stats, plot_min=33, plot_max=73):
     return out
 
 def boxplot(position_stats):
+    '''given stats for each position, make out a horizontal boxplot'''
     lines = [quality_header()]
     lines += ["%s %d" %(boxplot_line(stat), i+1) for i, stat in enumerate(position_stats)]
     return "\n".join(lines) + "\n"
 
 def quality_header():
+    '''boxplot header that shows quality score characters'''
     return "|" + "".join([chr(x+1) for x in range(33, 73+1)]) + "|"
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="get a fastq quality report")
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+            Get a fastq quality report. Every line is one position in the reads.
+
+            0 median
+            = within IQR
+            - within 1.5 IQR of Q1 or Q3
+            '''))
     parser.add_argument('fastq', help='input fastq')
     parser.add_argument('--output', '-o', default=sys.stdout, type=argparse.FileType('w'), help='output log (default stdout)')
     args = parser.parse_args()
 
-    stats = fastq_quality_dist(args.fastq)
+    stats = fastq_quality_stats(args.fastq)
     print boxplot(stats)
